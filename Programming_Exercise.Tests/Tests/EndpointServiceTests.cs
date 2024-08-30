f23bb42e948aeec122e97ed76bfead0a8e8b697c
Filipe Pimenta de Souza <fipisouza91@gmail.com>
2024-08-30 01:04:52 -0300

    Criação de testes unitarios para o programa

---
+++ Programming_Exercise.Tests/Tests/EndpointServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using Programming_Exercise.Models;
+using Programming_Exercise.Services;
+using System;
+
+namespace Programming_Exercise.Tests
+{
+    public class EndpointServiceTests
+    {
+        private readonly EndpointService _service;
+
+        public EndpointServiceTests()
+        {
+            _service = new EndpointService();
+        }
+
+        [Fact]
+        public void InsertEndpoint_ShouldAddNewEndpoint()
+        {
+            // Arrange
+            var endpoint = new Endpoint
+            {
+                SerialNumber = "123",
+                MeterModelId = 16,
+                MeterNumber = 1001,
+                FirmwareVersion = "1.0.0",
+                SwitchState = 1
+            };
+
+            // Act
+            _service.InsertEndpoint(endpoint);
+            var result = _service.FindEndpointBySerial("123");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("123", result.SerialNumber);
+        }
+
+        [Fact]
+        public void InsertEndpoint_ShouldThrowExceptionForDuplicateSerial()
+        {
+            // Arrange
+            var endpoint1 = new Endpoint { SerialNumber = "123" };
+            var endpoint2 = new Endpoint { SerialNumber = "123" };
+
+            // Act
+            _service.InsertEndpoint(endpoint1);
+
+            // Assert
+            Assert.Throws<Exception>(() => _service.InsertEndpoint(endpoint2));
+        }
+
+        // Adicione outros métodos de teste conforme necessário
+    }
+}
---
+++ Programming_Exercise.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Programming_Exercise.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
